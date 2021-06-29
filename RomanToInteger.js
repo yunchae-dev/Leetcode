acbabd3d7c44feecf03aa85298bc2d34e9b4e8a8
storicity-YC <yunchae.dev@gmail.com>
2021-06-29 23:26:20 +0900

    solution

--- RomanToInteger.js
+++ RomanToInteger.js
@@ -55,3 +55,24 @@
 // 1 <= s.length <= 15
 // s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
 // It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+// Top voted solution
+symbols = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+var romanToInt = function (s) {
+  value = 0;
+  for (let i = 0; i < s.length; i += 1) {
+    symbols[s[i]] < symbols[s[i + 1]]
+      ? (value -= symbols[s[i]])
+      : (value += symbols[s[i]]);
+  }
+  return value;
+};
